a1041f5c1c96a12c44ff0c2d7d0b7c51dec818bb
Laura Cintra <79454879+Laura-Cintra@users.noreply.github.com>
2025-03-21 12:19:52 -0300

    feat: category item

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,15 +1,26 @@
+import CategoryItem from "@/components/category-item";
 import Navbar from "@/components/nav-bar";
 
-export default function CategoriesPage() {
-    return (
-        <>
-            <Navbar active="categorias" />
+async function getCategories(){
+    const response = await fetch("http://localhost:8080/categories")
+    return await response.json()
+}
 
-            <main className="flex items-center justify-center">
-                <div className="bg-slate-900 rouded min-w-2/3 p-6 m-6">
-                    <h2 className="text-lg font-bold">Categorias</h2>
-                </div>
-            </main>
-        </>
+export default async function Categoriespage(){
+    const data: Array<Category> = await getCategories()
+
+    return(
+    <>
+    <Navbar active="categorias"/>
+
+    <main className="flex items-center justify-center">
+        <div className=" bg-slate-900 rounded min-w-2/3 p-5 m-6">
+            <h2 className="text-lg font-bold">Categorias</h2>
+
+            { data.map( category => <CategoryItem key={category.id} category={category} /> ) }
+            
+        </div>
+    </main>
+    </>
     )
 }
