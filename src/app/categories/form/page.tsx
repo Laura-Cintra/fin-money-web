1926ce381ca68d254b31dcce60f3e2a5a16279f3
Laura-Cintra <lauraocintra@gmail.com>
2025-03-27 11:57:47 -0300

    feat: category form

---
+++ src/app/categories/form/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { createCategory } from "@/actions/category-actions";
+import Navbar from "@/components/nav-bar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { useActionState } from "react";
+
+const initialState = {
+    values: {
+        name: "",
+        icon: ""
+    }
+}
+
+export default function Categoriespage(){
+    const[state, formAction, pendind] = useActionState(createCategory, initialState)
+
+    return(
+    <>
+    <Navbar active="categorias"/>
+
+    <main className="flex items-center justify-center">
+        <div className=" bg-slate-900 rounded p-5 m-6 max-w-[500px]">
+            <h2 className="font-bold">Cadastrar Categoria</h2>
+
+            <form action={formAction} className="space-y-4 mt-6">
+                <Input name="name" placeholder="Nome da categoria"/>
+                <Input name="icon" placeholder="Ícone"/>
+
+                <div className="flex justify-around">
+                    <Button variant={"outline"} asChild>
+                        <Link href={"/categories"}>Cancelar</Link>
+                    </Button>
+                    <Button>Salvar</Button>
+                </div>
+            </form>
+        </div>
+    </main>
+    </>
+    )
+}
