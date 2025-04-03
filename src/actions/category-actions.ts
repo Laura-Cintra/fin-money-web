bc609f2159fd3cffccbd83e4c5e7ddd6dc33a0ec
Laura Cintra <lauraocintra@gmail.com>
2025-04-02 22:34:49 -0300

    fix: category actions

--- src/actions/category-actions.ts
+++ src/actions/category-actions.ts
@@ -1,4 +1,3 @@
-import { error } from "console"
 import { redirect } from "next/navigation"
 const API_URL = "http://localhost:8080/categories"
 
@@ -8,28 +7,28 @@ export async function getCategories(){
 }
 
 export async function createCategory(initialState: any, formData: FormData) {
-    const data = {
-        name: formData.get("name"),
-        icon: formData.get("icon")
+    const dados = {
+      name: formData.get("name"),
+      icon: formData.get("icon"),
+    };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dados),
+    };
+    const response = await fetch(API_URL, options);
+    
+    // return early
+    if(!response.ok){
+      return {
+        errors: {
+          name: "nome é obrigatório",
+          icon: "tem que começar com maiuscula"
+        }
+      }
     }
     
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    }
-
-    const response = await fetch(API_URL, options)
-
-    if(response.ok){
-        return{
-            errors: {
-                name: "nome é obrigatório",
-                icon: "tem que começar com maiúscula"
-            }
-        }
-    }
     redirect("/categories")
 }
